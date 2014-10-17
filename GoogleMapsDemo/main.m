92a51e3a5f1b00a1cd934b7211cef7c65a1713f2
Akshay Bharath <akshay@animoto.com>
2014-10-17 14:02:41 -0400

    Initial Commit

---
+++ GoogleMapsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoogleMapsDemo
+//
+//  Created by Akshay Bharath on 10/17/14.
+//  Copyright (c) 2014 Akshay Bharath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
