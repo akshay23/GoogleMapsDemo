3b1c58db84ed468e633fa8b337424cf8b2aa0409
Akshay Bharath <akshay@animoto.com>
2014-11-19 11:40:12 -0500

    Added WatchKit targets
    - Extension and App targets

---
+++ MyFavPins WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  MyFavPins WatchKit Extension
+//
+//  Created by Akshay Bharath on 11/19/14.
+//  Copyright (c) 2014 Akshay Bharath. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
